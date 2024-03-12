d35782332e4348ef263c8c540e03a678bce661c7
aakashloyar <aakashloyar@gmail.com>
2024-03-12 23:09:22 +0530

    connected to database successfully

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const router=require("./routes/user")
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.use("/user",router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,24 @@
 const express = require('express')
 const router=express.Router
+const {User}=require('../db/index');
+router.post("/signup",async (req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const user=User.findOne();
+        if(user) {
+            res.status(403).json({message:"User alredy exists"});
+            return;
+        }
+        const newUser=new User({email,password});
+        await newUser.save();
 
+        console.log("created succesfully");
+
+    } catch(error) {
+        res.status(500).json({message:"server error"})
+        throw new Error(error);
+    }
+    
+})
 
 module.exports =router
